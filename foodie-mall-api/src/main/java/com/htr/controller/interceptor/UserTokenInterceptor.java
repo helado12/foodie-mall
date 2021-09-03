3f607a4ea2ea309b502bf5f0aba870b72c67476e
hhe233 <hhe6389@gmail.com>
2021-09-03 23:54:56 +0100

    Add interceptor return error message to front end

--- foodie-mall-api/src/main/java/com/htr/controller/interceptor/UserTokenInterceptor.java
+++ foodie-mall-api/src/main/java/com/htr/controller/interceptor/UserTokenInterceptor.java
@@ -1,5 +1,7 @@
 package com.htr.controller.interceptor;
 
+import com.htr.utils.HtrJSONResult;
+import com.htr.utils.JsonUtils;
 import com.htr.utils.RedisOperator;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * @Author: T. He
@@ -28,19 +32,43 @@ public class UserTokenInterceptor implements HandlerInterceptor {
         if (StringUtils.isNotBlank(userId) && StringUtils.isNotBlank(userToken)){
             String uniqueToken = redisOperator.get(REDIS_USER_TOKEN + ":" + userId);
             if (StringUtils.isBlank(uniqueToken)){
-                System.out.println("验证失败");
+                returnErrorResponse(response, HtrJSONResult.errorMsg("Login required"));
                 return false;
             }else {
                 if (!uniqueToken.equals(userToken)){
+                    returnErrorResponse(response, HtrJSONResult.errorMsg("Account is logged in at another location"));
                     return false;
                 }
             }
         }else {
-            System.out.println("验证失败");
+            returnErrorResponse(response, HtrJSONResult.errorMsg("Login required"));
             return false;
         }
-        System.out.println("验证成功");
+
         return true;
+    }
+
+    public void returnErrorResponse(HttpServletResponse response,
+                                    HtrJSONResult result){
+
+        OutputStream out = null;
+        try {
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("text/json");
+            out = response.getOutputStream();
+            out.write(JsonUtils.objectToJson(result).getBytes("utf-8"));
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (out != null){
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
